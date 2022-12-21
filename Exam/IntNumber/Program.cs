d46a4b41850cb74bd1ec1370dedf232bb277fa6c
itplachkova <35569721+itplachkova@users.noreply.github.com>
2022-12-21 11:18:44 +0200

    Exam

---
+++ Exam/IntNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte n1 = -100;
+            byte n2 = 128;
+            short n3 = -3540;
+            ushort n4 = 64876;
+            int n5 = 2147483647;
+            int n6 = -1141583228;
+            long n7 = -1223372036854775808;
+            Console.WriteLine($"{n1}, {n2}, {n3}, {n4}, {n5}, {n6}, {n7}");
+        }
+    }
+}
